6be90f252bb587a63b4775c05a3bbaf864e0c334
gksmf6611 <gksmf6611@gmail.com>
2021-02-17 17:44:41 +0900

    [chap01]sysinfo

---
+++ chap01-sysinfo/chap01-sysinfo.c
@@ -0,0 +1,37 @@
+﻿/*
+* chap01-sysinfo
+* 시스템 정보 확인
+*----------------------
+* sizeof(type) 내장함수(built-in function)
+
+*/
+
+
+
+#include <stdio.h>
+
+
+int main()
+{
+	printf(">>> system information <<<\n");
+
+	printf("sizeof(char)(%d)\n", sizeof(char));// 1바이트
+	printf("sizeof(short)(%d)\n", sizeof(short));//2바이트
+	printf("sizeof(int)(%d)\n", sizeof(int));//
+	printf("sizeof(long)(%d)\n", sizeof(long));
+	printf("sizeof(float)(%d)\n", sizeof(float));
+	printf("sizeof(double)(%d)\n", sizeof(double));
+	
+
+	printf("sizeof(char*)(%d)\n", sizeof(char*));// 1바이트
+	printf("sizeof(short*)(%d)\n", sizeof(short*));//2바이트
+	printf("sizeof(int*)(%d)\n", sizeof(int*));//
+	printf("sizeof(long*)(%d)\n", sizeof(long*));
+	printf("sizeof(float*)(%d)\n", sizeof(float*));
+	printf("sizeof(double*)(%d)\n", sizeof(double*));
+
+	printf("sizeof(void*)(%d)\n", sizeof(void*));
+
+}
+
+//바이트 단위로 정보를 알려줌 
