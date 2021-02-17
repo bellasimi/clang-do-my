5de9365329cf15f9806771ff5817570374bef081
gksmf6611 <gksmf6611@gmail.com>
2021-02-17 11:21:25 +0900

    .

---
+++ chap03-float.c
@@ -0,0 +1,23 @@
+﻿/*실수 자료형: float, double
+* ---------------------
+* 
+* float:4byte, 1.2E-38 ~3.4E38
+* double: 8byte, 2.2E-308
+* ------------------
+* 10= 0.1E10 E는 지수 제곱 승 
+* 100.0 = 0.1 
+*/
+
+#include <stdio.h>
+
+void main()
+{
+	//float pi = 3.141592f; //f를 붙여줘야 오류가 안남
+	double dpi = 3.141592789; //뒤에 F 안붙여도 됨14 자리
+	float pi = 3.141592789f;//6자리
+	float ti = 0.123e3;// e3 = 1000
+	printf("pi(%f)(%.10f)(%.10f)(%e)(%f)\n", pi,pi,dpi,ti,ti);
+
+
+
+}
---
+++ prepare.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+
+int sum(value1, value2)
+{
+	int result = value1 + value2;
+		return result;
+}
+
+void print(int x)
+{
+	printf("(d%)\n",x);
+
+}
+	
+void main(void)
+{
+	int result = sum(10, 2);
+	print(result);
+	print(10);
+
+	
+
+}
